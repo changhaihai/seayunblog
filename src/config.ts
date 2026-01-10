d5c82f18d43d600466d3d85401ee7a779b2d42fc
changhaihai <431249235@qq.com>
2026-01-10 22:44:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -101,7 +101,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "hhttps://em.zhixora.com/content/uploadfile/202601/84271767909250.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https://em.zhixora.com/content/uploadfile/202601/84271767909250.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Detector",
 	bio: "Protect What You Love.",
 	links: [
