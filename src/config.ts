295d4d36706ae36b9245ea28da641abd5f2e84f1
changhaihai <431249235@qq.com>
2026-01-11 16:15:48 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "/xinghui.avif", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "../content/uploadfile/871d1768119288.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
