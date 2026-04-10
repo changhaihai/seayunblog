b3109335f05d43831cd4bd22d3dd11b97c940a22
changhaihai <431249235@qq.com>
2026-04-10 21:51:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "https://sns-webpic-qc.xhscdn.com/202604102134/f797691e152295a495c2f54ef40f81bf/1040g2sg31dveio550kd05n188hqh95185922t7g!nd_dft_wgth_jpg_3", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "http://xpay.zhixora.com/static/images/login-bg/login-bg-19.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
